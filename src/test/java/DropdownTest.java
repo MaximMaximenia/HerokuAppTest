7d6edeaa571da7962642682fd973bf72101dfdb5
Admin <maxich66@gmail.com>
2020-08-07 14:08:30 +0300

    Add DropdownTest

---
+++ src/test/java/DropdownTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class DropdownTest {
+    WebDriver browser;
+    @Test
+    public void validateInput (){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        browser = new ChromeDriver();
+        browser.get("http://the-internet.herokuapp.com/dropdown");
+        Select select  = new Select(browser.findElement(By.id("dropdown")));
+        select.selectByVisibleText("Option 1");
+        String text = select.getFirstSelectedOption().getText();
+        Assert.assertEquals(text,"Option 1");
+        browser.close();
+    }}
+
