f0e41b21c7bb7cc6cf02932ccfd9cd7a75e38ca2
Admin <maxich66@gmail.com>
2020-08-07 15:04:51 +0300

    Add DataTablesTest

---
+++ src/test/java/DataTablesTest.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DataTablesTest {
+    WebDriver browser;
+    @Test
+    public void validateInput (){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        browser = new ChromeDriver(options);
+        browser.get("http://the-internet.herokuapp.com/tables");
+        String smithLastName =  browser.findElement(By.xpath("//table//tr[1]//td[1]")).getText();
+        String smithMail =  browser.findElement(By.xpath("//table//tr[1]//td[3]")).getText();
+        String smithFirstName =  browser.findElement(By.xpath("//table//tr[1]//td[2]")).getText();
+
+        String bachLastName =  browser.findElement(By.xpath("//table//tr[2]//td[1]")).getText();
+        String bachFirstName =  browser.findElement(By.xpath("//table//tr[2]//td[2]")).getText();
+        String bachMail =  browser.findElement(By.xpath("//table//tr[2]//td[3]")).getText();
+
+        Assert.assertEquals(smithFirstName,"John");
+        Assert.assertEquals(smithLastName,"Smith");
+        Assert.assertEquals(smithMail,"jsmith@gmail.com");
+        Assert.assertEquals(bachFirstName,"Frank");
+        Assert.assertEquals(bachLastName,"Bach");
+        Assert.assertEquals(bachMail,"fbach@yahoo.com");
+
+        browser.close();
+
+            }
+
+        }
+
